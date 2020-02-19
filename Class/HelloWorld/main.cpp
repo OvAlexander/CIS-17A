0d33993137cba2dbbd4012bca582ae5052fd85ee
OvAlexander <OvAlexander@users.noreply.github.com>
2020-02-18 18:06:59 -0800

    files added

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.cpp
+ * Author: Alexander Ov
+ * Created on February 18, 2020, 5:58 PM
+ * Purpose: Hello World Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+   //Set Random Number seed
+
+   //Declare Variable Data Types and Constants
+
+   //Initialize Variables
+
+   //Process or map Inputs to Outputs
+
+   //Display Outputs
+   cout<<"Hello World"<<endl;
+   cout<<"Hello World";
+   cout<<endl;
+   cout<<"Hello ";
+   cout<<"World";
+   cout<<endl;
+   //Exit stage right!
+   return 0;
+}
